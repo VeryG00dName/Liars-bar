aa452aa108520a33a31e646410644efa14629fc7
VeryGoodName <verygoodnamework@gmail.com>
2025-02-03 17:10:57 -0800

    Update play_vs_ai.py

--- src/misc/play_vs_ai.py
+++ src/misc/play_vs_ai.py
@@ -9,7 +9,7 @@ from tkinterdnd2 import TkinterDnD, DND_FILES
 import torch
 import numpy as np
 from src.env.liars_deck_env_core import LiarsDeckEnv
-from src.model.models import PolicyNetwork, OpponentBehaviorPredictor
+from src.model.new_models import PolicyNetwork, OpponentBehaviorPredictor
 from src import config
 
 logging.basicConfig(level=logging.INFO)
