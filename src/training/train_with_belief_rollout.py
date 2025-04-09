93792f671802000dec8fe1de860bee1313b48136
VeryGoodName <verygoodnamework@gmail.com>
2025-04-09 10:27:16 -0700

    Update train_with_belief_rollout.py

--- src/training/train_with_belief_rollout.py
+++ src/training/train_with_belief_rollout.py
@@ -181,7 +181,7 @@ def train_with_belief_space_policy(env, device, num_episodes=10000, load_checkpo
     training_agent = 'player_0'
     opponent_agents = ['player_1', 'player_2']
     
-    sample_obs = env.observe(env.agents[0], new=True)[env.agents[0]]
+    sample_obs = env.observe(env.agents[0], newer=True)[env.agents[0]]
     obs_dim = sample_obs.shape[0]
     action_dim = env.action_spaces[env.agents[0]].n
     
@@ -418,7 +418,7 @@ def train_with_belief_space_policy(env, device, num_episodes=10000, load_checkpo
                 env.step(None)
                 continue
             
-            observation_dict_new = env.observe(agent, new=True)
+            observation_dict_new = env.observe(agent, newer=True)
             observation_new = observation_dict_new[agent]
             observation_dict_old = env.observe(agent, new=False)
             observation_old = observation_dict_old[agent]
